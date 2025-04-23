d2c9ed13baeca4de012129fde6ea91e995750004
Ade Alleyne-Forte <ade.alleyne-forte@olympian.org>
2025-04-22 22:09:24 -0500

    Update GasMileage.java

--- GasMileage.java
+++ GasMileage.java
@@ -9,6 +9,14 @@ package com.mycompany.gasmileage;
  * @author ade_f
  */
 
+
+//4.17 (Gas Mileage) Drivers are concerned with the mileage their automobiles get. 
+//One driver has kept track of several trips by recording the miles driven and gallons used for each tankful. 
+//Develop a Java application that will input the miles driven and gallons used (both as integers) for each trip. 
+//The program should calculate and display the miles per gallon obtained for each trip and print the combined miles per gallon obtained for all trips up to this point. 
+//All averaging calculations should produce floating-point results. Use class  and sentinel-controlled iteration to obtain the data from the user.
+
+
 //Name: Ade Alleyne-Forte
 
 //Course: ITSE 2321_HW4
