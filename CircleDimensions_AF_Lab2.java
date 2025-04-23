007afa397486688cd0ae5d08b44a0fafc94440c3
Ade Alleyne-Forte <ade.alleyne-forte@olympian.org>
2025-04-22 22:14:16 -0500

    Update CircleDimensions_AF_Lab2.java

--- CircleDimensions_AF_Lab2.java
+++ CircleDimensions_AF_Lab2.java
@@ -8,6 +8,17 @@ package com.mycompany.circledimensions;
  *
  * @author ade_f
  */
+
+//2.28 (Diameter, Circumference and Area of a Circle) Here’s a peek ahead. 
+//In this chapter, you learned about integers and the type . 
+//Java can also represent floating-point numbers that contain decimal points, such as 3.14159. 
+//Write an application that inputs from the user the radius of a circle as an integer and prints the circle’s diameter, circumference and area using the floating-point value 3.14159 for π. 
+//Use the techniques shown in Fig. 2.7. [Note: You may also use the predefined constant for the value of π. This constant is more precise than the value 3.14159.
+//Class is defined in package . Classes in that package are imported automatically, so you do not need to import class to use it.] 
+//Use the following formulas (r is the radius): Do not store the results of each calculation in a variable. Rather, specify each calculation as the value that will be output in a statement. The values produced by the circumference and area calculations are floating-point numbers. 
+//Such values can be output with the format specifier in a statement. You’ll learn more about floating-point numbers in Chapter 3.
+
+
 //Name: Ade Alleyne-Forte
 
 //Course: ITSE 2321
