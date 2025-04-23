4a8ec55be583acd86f935f4f3fa6fc106e0f0cee
Ade Alleyne-Forte <ade.alleyne-forte@olympian.org>
2025-04-22 22:22:18 -0500

    Update PalindromeChecker_AF_Lab4.java

--- PalindromeChecker_AF_Lab4.java
+++ PalindromeChecker_AF_Lab4.java
@@ -9,6 +9,14 @@ package com.mycompany.palindromechecker;
  * @author ade_f
  */
 
+//4.21 (Find the Largest Number) The process of finding the largest value is used frequently in computer applications. 
+//For example, a program that determines the winner of a sales contest would input the number of units sold by each salesperson. 
+//The salesperson who sells the most units wins the contest. 
+//Write a pseudocode program, then a Java application that inputs a series of 10 integers and determines and prints the largest integer. 
+//Your program should use at least the following three variables: 
+//: A counter to count to 10 (i.e., to keep track of how many numbers have been input and to determine when all 10 numbers have been processed). 
+//: The integer most recently input by the user. : The largest number found so far.
+
 import java.util.Scanner;
 
 public class PalindromeChecker {
