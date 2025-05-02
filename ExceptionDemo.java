bd710c8a5ba7d2da6e1fa2a58158c4bb504bfe9a
Ade Alleyne-Forte <ade.alleyne-forte@olympian.org>
2025-05-01 20:03:01 -0500

    Add files via upload

---
+++ ExceptionDemo.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exceptiona;
+
+/**
+ *
+ * @author ade_f
+ */
+
+//Name: Ade Alleyne-Forte
+
+//Course: ITSE 2321_HW11
+
+//Program Status: Complete
+
+// Description: The following code builds the ExceptionTest main class
+
+// Import necessary class
+
+import java.io.IOException;
+
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            // Uncomment one block at a time to test different exceptions
+
+            // Throw custom ExceptionA
+            throw new ExceptionA("This is ExceptionA.");
+            
+            // Throw custom ExceptionB
+            // throw new ExceptionB("This is ExceptionB.");
+            
+            // Throw NullPointerException (built-in exception)
+            // throw new NullPointerException("This is a NullPointerException.");
+            
+            // Throw IOException (built-in exception)
+            // throw new IOException("This is an IOException.");
+
+        } catch (Exception exception) {
+            // Catch any type of Exception
+            System.out.println("Caught exception: " + exception.getMessage());
+        }
+    }
+}
